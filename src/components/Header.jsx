54950a708a2a394f4d1944232d48f9e31b9032c9
fatemeh <fatemehdordkeshan@yahoo.com>
2023-11-25 16:31:08 +0330

    add activebar style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+
 const filters = ["All", "Completed", "Upcoming", "Active"];
 function Header() {
+  const [activeTab, setActiveTab] = useState("All");
+  const handleActiveTab = (status) => {
+    setActiveTab(status);
+    console.log(activeTab);
+  };
   return (
     <header className="header">
       <h1 className="header__title">My Courses(3)</h1>
       <ul className="status-bar">
         {filters.map((f, index) => {
           return (
-            <li className="filter-item" key={index}>
-              {f}
+            <li className="filter-item">
+              <button
+                className={f === activeTab ? "active" : ""}
+                key={f}
+                onClick={() => handleActiveTab(f)}
+              >
+                {f}
+              </button>
             </li>
           );
         })}
